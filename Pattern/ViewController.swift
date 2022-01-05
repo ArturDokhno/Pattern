8ad8f906eca6c183a0ea05aeb66f31d160e3ecab
ArturDokhno <artur.dohno@icloud.com>
2022-01-05 17:06:12 +0500

    Initial Commit

---
+++ Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pattern
+//
+//  Created by Артур Дохно on 05.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
